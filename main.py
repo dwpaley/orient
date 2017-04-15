f30c7f4f9dd430d12470fcc2d54c7ace8ea46595
dwpaley <dwpaley@gmail.com>
2017-04-15 14:06:48 -0400

    fixed counter for saved filenames

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/local/bin/python3
-import pdb
 
 import proc_file, metric
 from subprocess import Popen, PIPE
@@ -13,8 +12,7 @@ def main(fileName, ntrial):
     CLtext = ['shelxl', fileName.rstrip('.ins') + '_orient']
     best, n, count = 1, 0, 0
 
-    pdb.set_trace()
-    for n in range(ntrial):
+    for trial in range(ntrial):
         proc_file.proc_file(fileName, matrices)
         p = Popen(CLtext, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         stdout, stderr = p.communicate()
